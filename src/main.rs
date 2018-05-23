2e5f1d44e93f5eaf36ec6ae9e35c1fa42793ab12
Mattias Cibien <mattias@mattiascibien.net>
2018-05-23 11:53:37 +0200

    feat: added logging and dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+extern crate dotenv;
+
+use dotenv::dotenv;
+
+use std::{thread,time};
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    dotenv().ok();
+
+    loop {
+        info!("Running");
+        try_retweet();
+        thread::sleep(time::Duration::from_secs(5*60));
+    }
+}
+
+fn try_retweet() {
+
 }
